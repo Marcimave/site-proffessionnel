0cc0eecf3dd85986b8583848341e51c5b9a97073
Marcimave <marcimavegithub@gmail.com>
2026-04-27 13:08:02 +0100

    Formulaire WhatsApp direct

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ document.getElementById("whatsappForm").addEventListener("submit", function(e) {
   const service = document.getElementById("service").value;
   const message = document.getElementById("message").value;
 
-  const phone = "241065158925 ;
+  const phone = "241065158925" ;
   const url = `https://wa.me/${phone}?text=` +
     `Bonjour, je m'appelle ${nom}.%0A` +
     `Je veux : ${service}.%0A` +
