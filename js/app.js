8548c2f8e01f5c857a0e1ae7595e010c54c189fd
Marcimave <marcimavegithub@gmail.com>
2026-04-27 13:55:45 +0100

    Formulaire WhatsApp PRO

--- js/app.js
+++ js/app.js
@@ -1,15 +1,19 @@
-document.getElementById("whatsappForm").addEventListener("submit", function(e) {
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e) {
   e.preventDefault();
 
   const nom = document.getElementById("nom").value;
   const service = document.getElementById("service").value;
   const message = document.getElementById("message").value;
 
-  const phone = "241065158925" ;
-  const url = `https://wa.me/${phone}?text=` +
-    `Bonjour, je m'appelle ${nom}.%0A` +
-    `Je veux : ${service}.%0A` +
-    `Message : ${message}`;
+  const phone = "241065158925"; 
+
+  const texte = `Bonjour, je m'appelle ${nom}.
+Je souhaite : ${service}.
+Projet : ${message}`;
+
+  const url = "https://wa.me/" + phone + "?text=" + encodeURIComponent(texte);
 
   window.open(url, "_blank");
 });
