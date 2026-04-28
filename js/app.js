82cc999d89e2d26d2d5e5aa568ac0e86a2e68a55
Marcimave <marcimavegithub@gmail.com>
2026-04-28 01:23:16 +0100

    Supression js

--- js/app.js
+++ js/app.js
@@ -25,26 +25,6 @@ title.innerHTML = "";
 
 let i = 0;
 
-/*
-function typing() {
-  if (i < text.length) {
-    const span = document.createElement("span");
-    span.textContent = text[i];
-
-    // garde les espaces visibles
-    if (text[i] === " ") {
-      span.innerHTML = "&nbsp;";
-    }
-
-    title.appendChild(span);
-    i++;
-    setTimeout(typing, 40);
-  }
-}
-
-typing();
-*/
-
   // Confirmation avant ouverture
   const confirmation = confirm("Vous allez être redirigé vers WhatsApp. Continuer ?");
 
